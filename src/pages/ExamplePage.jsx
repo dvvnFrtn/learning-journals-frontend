ac8eabc9f5eff6c6fb91fd9f255480782677ff9b
dvvnFrtn <muhrizkifajar28@gmail.com>
2024-11-26 04:57:27 -0500

    feat: add example page

---
+++ src/pages/ExamplePage.jsx
@@ -0,0 +1,42 @@
+import { PlusIcon } from "@heroicons/react/20/solid";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import Option from "../components/Option";
+import DashboardLayout from "../layouts/DashboardLayout";
+
+const ExamplePage = () => {
+    const people = [
+        { id: 1, name: 'Durward Reynolds' },
+        { id: 2, name: 'Kenton Towne' },
+        { id: 3, name: 'Therese Wunsch' },
+        { id: 4, name: 'Benedict Kessler' },
+        { id: 5, name: 'Katelyn Rohan' },
+    ]
+
+    return (
+        <DashboardLayout>
+            <div className="flex w-full gap-2 flex-col">
+                <div className="flex justify-between items-center">
+                    <h1 className="font-bold text-6xl text-slate-900">User</h1>
+                    <Button
+                        variant="primary"
+                    >
+                        <PlusIcon className="size-6 fill-white" />
+                        <span>Hello</span>
+                    </Button>
+                </div>
+                <p className="text-slate-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <Input
+                label="Journal"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                name="journal"
+                type="text"
+                placeholder="Type here.."
+            />
+            <Option name="list" options={people} />
+        </DashboardLayout>
+    )
+}
+
+export default ExamplePage;
